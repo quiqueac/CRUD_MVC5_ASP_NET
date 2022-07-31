e90e7035ec0129a8327f5b6458f37a134b18ada7
Enrique <enriquenada@outlook.es>
2022-07-31 18:58:47 -0500

    Inicio

---
+++ CRUD_MVC5_ASP_NET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_MVC5_ASP_NET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
